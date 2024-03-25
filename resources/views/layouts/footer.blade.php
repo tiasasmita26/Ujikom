5882f89b49175d7fc66c477091f722b0a88d1af3
unknown <sasmitatia74@gmail.com>
2024-03-25 13:01:51 +0700

    front-end

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,9 @@
+<div id="templatemo_footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 text-center">
+                <p>Copyright &copy; 2084 Your Company TS</p>
+            </div> <!-- /.col-md-12 -->
+        </div> <!-- /.row -->
+    </div> <!-- /.container -->
+</div> <!-- /.templatemo_footer -->
---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,40 @@
+<div class="site-header">
+    <div class="main-navigation">
+        <div class="responsive_menu">
+            <ul>
+                <li><a class="show-1 templatemo_home" href="#">Gallery</a></li>
+                <li><a class="show-2 templatemo_page2" href="#">PerPaketan</a></li>
+                <li><a class="show-3 templatemo_page3" href="#">Services</a></li>
+                <li><a class="show-4 templatemo_page4" href="#">About Us</a></li>
+                <li><a class="show-5 templatemo_page5" href="#">Kontak</a></li>
+            </ul>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 responsive-menu">
+                    <a href="#" class="menu-toggle-btn">
+                        <i class="fa fa-bars"></i>
+                    </a>
+                </div> <!-- /.col-md-12 -->
+                <div class="col-md-12 main_menu">
+                    <ul>
+                        <li><a class="show-1 templatemo_home" href="#">Gallery</a></li>
+                        <li><a class="show-2 templatemo_page2" href="#">PerPaketan</a></li>
+                        <li><a class="show-3 templatemo_page3" href="#">Services</a></li>
+                        <li><a class="show-4 templatemo_page4" href="#">About Us</a></li>
+                        <li><a class="show-5 templatemo_page5" href="#">Kontak</a></li>
+                    </ul>
+                </div> <!-- /.col-md-12 -->
+            </div> <!-- /.row -->
+        </div> <!-- /.container -->
+    </div> <!-- /.main-navigation -->
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 text-center">
+                <a href="#" class="templatemo_logo">
+                    <img src="/images/gallery/logoAW.png" style="height:250px; weight:100px" alt="">
+                </a> <!-- /.logo -->
+            </div> <!-- /.col-md-12 -->
+        </div> <!-- /.row -->
+    </div> <!-- /.container -->
+</div> <!-- /.site-header -->
