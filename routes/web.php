20bf2eb33d1df568c3f104a8cfc0d620ac2c7cbf
unknown <sasmitatia74@gmail.com>
2024-03-27 13:19:01 +0700

    tampilan login

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,5 +15,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('main');
+    return view('login');
 });
+
